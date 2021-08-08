02b0181aa75255b385c3083124ab27194c373d88
kom50 <82828406+kom50@users.noreply.github.com>
2021-08-08 18:26:17 +0530

    u

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const load = () => {
-    console.log('Load')
     let width = window.innerWidth;
     let height = window.innerHeight;
     let doc = document.querySelector('.container');
@@ -9,10 +8,8 @@ const load = () => {
         'marginLeft': '6px',
         'justify-content': 'center',
     })
-    console.log(width, height)
 }
 window.addEventListener('resize', load);
-
 
 window.onload = () => {
     //
@@ -28,7 +25,6 @@ window.onload = () => {
 
     shuffle =document.querySelector('.shuffle')
     repeat =document.querySelector('.repeat')
-
     // 
     firstPage = document.querySelector('.inner-container:first-child');
     songListPage = document.querySelector('.song-list-page');
@@ -36,7 +32,6 @@ window.onload = () => {
 
     //  animation
     let roundCircle = document.querySelector('.round-circle');
-   
 
     let audioFiles = [],
         songNumber = 0,
@@ -45,32 +40,14 @@ window.onload = () => {
     files.oninput = function (event) {
         let files = event.target.files;
         totalSongs = files.length;
-        console.log('files')
-        console.log(files)
-        console.log(event.target.files)
 
         for (i = 0; i < totalSongs; i++) {
             audioFiles[i] = URL.createObjectURL(files[i])
             songsName[i] = files[i].name;
         }
-
         audio.src = audioFiles[0];
         songName.textContent = songsName[0]
-
-        // console.log(audioFiles)
-        // console.log(songsName)
-        // console.log("audio")
-        // console.log(audio)
-        // console.log(audio.duration)
     }
-
-    // audio.onplaying = function (event) {
-    //     console.log('duration ', event)
-    //     console.log('duration ', event.target.duration)
-    //     console.log('duration ', event.target)
-
-      
-    // }
 
     let isPlay = true;
     play.onclick = function (event) {
@@ -91,10 +68,8 @@ window.onload = () => {
         } else {
             songNumber--;
         }
-        // console.log(songNumber);
         audio.src = audioFiles[songNumber];
         songName.textContent = songsName[songNumber]
-
     }
 
     next.onclick = function (event) {
@@ -103,14 +78,12 @@ window.onload = () => {
         } else {
             songNumber++;
         }
-        // console.log(songNumber);
         audio.src = audioFiles[songNumber];
         songName.textContent = songsName[songNumber]
     }
 
     shuffle.addEventListener('click', (event) => {
         rand = Math.round(Math.random() * (totalSongs - 1));
-        console.log(rand);
         songNumber = rand;
         audio.src = audioFiles[songNumber];
         songName.textContent = songsName[songNumber]
@@ -124,11 +97,9 @@ window.onload = () => {
             song.innerHTML = `${index + 1}.   ${songName}`;
             
             song.addEventListener('click',(event) =>{
-                console.log('song  clicked', event.target.id)
                 songNumber = event.target.id;
                 audio.src = audioFiles[songNumber];
                 songName.textContent = songsName[songNumber]
-
                 event.stopPropagation();
             })
             songList.appendChild(song);
@@ -147,9 +118,6 @@ window.onload = () => {
              firstPage.style.display = 'flex'
              songListPage.style.display = 'none'
          }
-
-        //  console.log('target', event.target)
-        //  console.log('currentTarget', event.currentTarget)
     }, false)
 
     roundCircle.onclick = (event) => {
@@ -157,7 +125,6 @@ window.onload = () => {
         firstPage.style.display = 'none'
         songListPage.style.display = 'flex'
     }
-
 }  // window.onload() method
 
 
